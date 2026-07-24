925baa6069be7cca87a98ea2dc62916437f49b47
ghulepranita9-afk <ghulepranita9@gmail.com>
2026-07-24 12:31:01 +0530

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,6 @@
+list=["python","dsa","dbms","paI","shd"]
+print("My list is=",list)
+list.append("economics")
+print("after adding new list",list)
+list.remove("python")
+print("after removing a item",list)
