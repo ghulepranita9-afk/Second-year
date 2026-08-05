fe767e03209797d90c8b0f73f6961c7dcaf3324b
ghulepranita9-afk <ghulepranita9@gmail.com>
2026-08-05 09:15:28 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,40 @@
+print("hello world")
+fruits=["apple","kiwi","apple"]
+print(fruits)
+print(fruits[1:2])
+fruit=("apple","kiwi","apple")
+print(fruit)
+no=(1,3,2,1,2)
+print(no)
+set={"saee",2,"ghule","saee"}
+print(set)
+animal={ 
+"reptiles":"snake",
+"mammals":"whale",
+"amphibian":"frogs"
+}
+print(animal.keys())
+print(animal.get("reptiles")) # it print value present at that key
+print(animal.get("snake")) # it print a none 
+list=["saee","khushi","devki","swaraj"]
+print(list.append("prathamesh")) # add prathmesh in list
+print(list)
+list.sort() # after sorting 
+print(list)
+print(list.pop(1)) # remove khushi
+print(list)
+list.insert(1,"shreya") # insert shreya
+print(list)
+list[4]="sanskar"
+print(list[4])
+a=len([1,2,3])
+print(a)
+b=["Hiii","welcome"]*4
+print(b)
+c=4 not in [1,2,3,4,7]
+print(c)
+
+tup=("chem","maths",23,45,"python",10,10,10) 
+print(tup.index(10))
+print(tup.count(10))
+del tup
